c26573499d4af67300a20fd7f43f14a0e33ff56c
Suphachai Hestmark <top_hestmark@yahoo.no>
2020-09-04 11:37:10 +0200

    cleaned up some codes

--- js/question3.js
+++ js/question3.js
@@ -3,16 +3,23 @@ const detailsContainer = document.querySelector(".detailsContainer");
 const queryString = document.location.search;
 const params = new URLSearchParams(queryString);
 const id = params.get("id");
-
 const url = "https://api.rawg.io/api/games/" + id;
 
-
 async function getDetails() {
-
   try {
     const response = await fetch(url);
     const details = await response.json();
 
+    createDetailsHtml(details);
+  }
+  catch(error) {
+    detailsContainer.innerHTML = displayError("An error occured when calling API");
+  }
+}
+getDetails()
+
+
+function createDetailsHtml(details) {
     const getDate = new Date(details.released);
     const ye = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(getDate)
     const mo = new Intl.DateTimeFormat('en', { month: 'long' }).format(getDate)
@@ -26,10 +33,4 @@ async function getDetails() {
     <div class="details_description">${details.description}</div>
     <p><strong>Date released:</strong> <time class="details-date">${dateReleased}</time></p>
     </div>`
-  }
-  catch(error) {
-    detailsContainer.innerHTML = displayError("An error occured when calling API");
-  }
 }
-
-getDetails()
